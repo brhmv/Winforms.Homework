d00df2f99684f99ad5a55a7bed0fd7aa54e04799
brhmv <allahverdiibrahimov2004@gmail.com>
2023-02-09 18:40:12 +0400

    first task

---
+++ Net forms/Net forms/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Net_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Baku = new System.Windows.Forms.Button();
+            this.btn_London = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Baku
+            // 
+            this.btn_Baku.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_Baku.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Baku.Location = new System.Drawing.Point(184, 49);
+            this.btn_Baku.Name = "btn_Baku";
+            this.btn_Baku.Size = new System.Drawing.Size(158, 44);
+            this.btn_Baku.TabIndex = 0;
+            this.btn_Baku.Text = "B A K U";
+            this.btn_Baku.UseVisualStyleBackColor = false;
+            this.btn_Baku.Click += new System.EventHandler(this.btn_Baku_Click);
+            // 
+            // btn_London
+            // 
+            this.btn_London.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_London.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_London.Location = new System.Drawing.Point(507, 49);
+            this.btn_London.Name = "btn_London";
+            this.btn_London.Size = new System.Drawing.Size(158, 44);
+            this.btn_London.TabIndex = 1;
+            this.btn_London.Text = "L O N D O N";
+            this.btn_London.UseVisualStyleBackColor = false;
+            this.btn_London.Click += new System.EventHandler(this.btn_London_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(898, 535);
+            this.Controls.Add(this.btn_London);
+            this.Controls.Add(this.btn_Baku);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Baku;
+        private System.Windows.Forms.Button btn_London;
+    }
+}
+
---
+++ Net forms/Net forms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Net_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BackgroundImage = new Bitmap(@"C:\Users\user\OneDrive\Desktop\Baku.jpg");
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btn_Baku_Click(object sender, EventArgs e)
+        {
+            BackgroundImage = new Bitmap(@"C:\Users\user\OneDrive\Desktop\Baku.jpg");
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btn_London_Click(object sender, EventArgs e)
+        {
+            BackgroundImage = new Bitmap(@"C:\Users\user\OneDrive\Desktop\London.jpeg");
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+    }
+}
